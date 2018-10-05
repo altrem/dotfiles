f81e71ff42faa686dae3475d22be920bb2edce7a
Alexandre Tremblay <alexandre.tremblay@protonmail.com>
2018-10-05 15:44:50 -0400

    Update dotfiles for rofi

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -3,6 +3,8 @@ import XMonad.Config.Desktop
 import XMonad.Hooks.DynamicLog
 import XMonad.Util.Run
 
+import qualified Data.Map as M
+
 main = do
     xmproc <- spawnPipe "xmobar"
     xmonad $ desktopConfig {
@@ -11,5 +13,8 @@ main = do
 	logHook  = dynamicLogWithPP xmobarPP {
 	    ppOutput = hPutStrLn xmproc,
 	    ppTitle  = xmobarColor "green" "" . shorten 50
-	}
+	},
+	keys = \c -> myKeys c `M.union` keys defaultConfig c
     }
+    where
+        myKeys (XConfig {modMask = modm}) = M.fromList $ [ ((modm, xK_r), spawn "rofi -show run") ]

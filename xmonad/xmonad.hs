d133a075addb341a27006bddd0f663e970eb22ff
Alexandre Tremblay <alexandre.tremblay@protonmail.com>
2018-10-06 13:20:10 -0400

    Update xmonad to include gaps

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -2,6 +2,8 @@ import XMonad
 import XMonad.Config.Desktop
 import XMonad.Hooks.DynamicLog
 import XMonad.Util.Run
+import XMonad.Layout.Spacing
+import XMonad.Hooks.ManageDocks
 
 import qualified Data.Map as M
 
@@ -14,7 +16,12 @@ main = do
 	    ppOutput = hPutStrLn xmproc,
 	    ppTitle  = xmobarColor "green" "" . shorten 50
 	},
+	layoutHook = myCustomLayout,
 	keys = \c -> myKeys c `M.union` keys defaultConfig c
     }
+    xmonad $ docks def
     where
         myKeys (XConfig {modMask = modm}) = M.fromList $ [ ((modm, xK_r), spawn "rofi -show run") ]
+
+myCustomLayout = spacingRaw True (Border 0 10 10 10) True (Border 10 10 10 10) True $ avoidStruts (tall ||| Mirror tall) 
+    where tall = Tall 1 (3/100) (1/2)

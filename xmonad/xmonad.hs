46ccdc0500f063db4e2e0906b6f1857553a287c4
Alexandre Tremblay <alexandre.tremblay@protonmail.com>
2018-10-08 12:13:37 -0400

    Update xmonad files

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -14,14 +14,16 @@ main = do
 	modMask  = mod4Mask,
 	logHook  = dynamicLogWithPP xmobarPP {
 	    ppOutput = hPutStrLn xmproc,
-	    ppTitle  = xmobarColor "green" "" . shorten 50
+	    ppTitle  = xmobarColor "#c89544" "" . shorten 50
 	},
 	layoutHook = myCustomLayout,
+	focusedBorderColor = myFocusedBorderColor,
 	keys = \c -> myKeys c `M.union` keys defaultConfig c
     }
     xmonad $ docks def
     where
-        myKeys (XConfig {modMask = modm}) = M.fromList $ [ ((modm, xK_r), spawn "rofi -show run") ]
+        myKeys (XConfig {modMask = modm}) = M.fromList $ [ ((modm, xK_r), spawn "rofi -show run"), ((modm, xK_Print), spawn "imgur-screenshot") ]
+	myFocusedBorderColor = "#c89544"
 
 myCustomLayout = spacingRaw True (Border 0 10 10 10) True (Border 10 10 10 10) True $ avoidStruts (tall ||| Mirror tall) 
     where tall = Tall 1 (3/100) (1/2)
